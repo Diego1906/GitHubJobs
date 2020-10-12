935af5b09f2f638025b38ca1b80de28946779bb7
Diego <di_fsantos@yahoo.com.br>
2020-10-11 22:56:53 -0300

    Create structure package initial

--- app/src/main/java/com/android/githubjobs/presentation/view/MainActivity.kt
+++ app/src/main/java/com/android/githubjobs/presentation/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.githubjobs
+package com.android.githubjobs.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.githubjobs.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
