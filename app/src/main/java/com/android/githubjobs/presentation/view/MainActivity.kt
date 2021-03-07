15490e204f55b72bfbda7e4cda2607d28e1489cf
diegofarias <k2.diegof@grupofleury.com.br>
2021-03-06 22:07:30 -0300

    Alteracao da lógica de preenchimento do recycler view de items para o start fragment

--- app/src/main/java/com/android/githubjobs/presentation/view/MainActivity.kt
+++ app/src/main/java/com/android/githubjobs/presentation/view/MainActivity.kt
@@ -1,40 +1,15 @@
 package com.android.githubjobs.presentation.view
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.android.githubjobs.R
-import com.android.githubjobs.presentation.viewmodel.JobsViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: JobsViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        initViewModel()
-        setListeners()
-    }
-
-    private fun initViewModel() {
-        viewModel.jobs.observe(this, Observer { jobs ->
-            jobs.let { list ->
-                list.forEach {
-                    println("Teste Jobs: $it")
-                }
-            }
-        })
-    }
-
-    private fun setListeners() {
-        btn_click.setOnClickListener {
-            viewModel.getAll()
-        }
     }
 }
